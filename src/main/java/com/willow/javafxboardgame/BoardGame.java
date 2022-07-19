c3fa76c0a256301c617d231892d7e8d6225f18d7
willowsenator <willowsenator@gmail.com>
2022-07-19 21:43:52 +0200

    add background images and rest

--- src/main/java/com/willow/javafxboardgame/BoardGame.java
+++ src/main/java/com/willow/javafxboardgame/BoardGame.java
@@ -8,10 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -19,6 +18,8 @@ import javafx.stage.StageStyle;
 import java.util.Objects;
 
 public class BoardGame extends Application {
+    private BackgroundImage uiBackgroundImage;
+    private Background uiBackground;
     private Group root;
     private Group gameBoard;
     private Scene scene;
@@ -31,11 +32,20 @@ public class BoardGame extends Application {
     private Image legalLayer;
     private Image creditLayer;
     private Image scoreLayer;
+    private Image backPlate;
     private Button gameButton;
     private Button helpButton;
     private Button legalButton;
     private Button creditButton;
     private Button scoreButton;
+
+    private Text playText;
+    private Text moreText;
+    private Text helpText;
+    private Text cardText;
+    private Text copyrightText;
+    private Text creditText;
+    private Text codeText;
     @Override
     public void start(Stage stage) {
         loadImageAssets();
@@ -71,6 +81,7 @@ public class BoardGame extends Application {
         scene.setFill(Color.TRANSPARENT);
         uiLayout = new StackPane();
         uiLayout.setBackground(Background.EMPTY);
+        uiLayout.setBackground(uiBackground);
         boardGameBackPlate = new ImageView();
         boardGameBackPlate.setImage(splashScreen);
         infoOverlay = new TextFlow();
@@ -97,11 +108,31 @@ public class BoardGame extends Application {
 
     private void loadImageAssets() {
 
+        backPlate = new Image(Objects.requireNonNull(getClass().getResource("/images/backplate.png").toString()),
+                1280, 640, true, false, true);
         splashScreen = new Image(Objects.requireNonNull(getClass().getResource("/images/welcome.png")).toString(),
                 1280, 640, true, false, true);
+        helpLayer = new Image(Objects.requireNonNull(getClass().getResource("/images/instructions.png")).toString(),
+                1280, 640, true, false, true);
+        legalLayer = new Image(Objects.requireNonNull(getClass().getResource("/images/copyrights.png")).toString(),
+                1280, 640, true, false, true);
+        creditLayer = new Image(Objects.requireNonNull(getClass().getResource("/images/credits.png")).toString(),
+                1280, 640, true, false, true);
+        scoreLayer = new Image(Objects.requireNonNull(getClass().getResource("/images/high-scores.png")).toString(),
+                1280, 640, true, false, true);
+
+        uiBackgroundImage = new BackgroundImage(backPlate, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        uiBackground = new Background(uiBackgroundImage);
     }
 
     private void createTextAssets() {
+        playText = new Text("Press the Start Game Button to Start!\n");
+        moreText = new Text("Use other buttons for instructions, copyrights, credits and high scores.");
+        helpText = new Text("To play game roll dice, advance gamepiece, follow gameboard instruction.");
+        cardText = new Text("If you land on square that requires card draw it will appear in UI area.");
+        creditText = new Text("Digital Imaging, 3D Modeling, 3D Texture Mapping, by Omar Fernando Moreno Benito. \n");
+        codeText = new Text("Game Design, User Interface Design, Java Programming by Omar Fernando Moreno Benito.");
     }
 
 
